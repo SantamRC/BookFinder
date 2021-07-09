f398cbaf677feb9094cf8f80b830ce8043bf38a1
SantamRC <santamdev404@gmail.com>
2021-07-09 22:16:00 +0530

    Added 2 routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Add from './Components/Add/Add'
+import Landing from './Components/Landing/Landing'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/add'>
+            <Add />
+          </Route>
+          <Route path='/landing'>
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link,useHistory} from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  const history=useHistory();
+
+  const goLanding=()=>{
+    history.push('/')
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography onClick={()=>goLanding()} variant="h6" className={classes.title}>
+            Book Finder
+          </Typography>
+          <Button component={Link} to={'/add'} color="inherit">Add New Book</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
