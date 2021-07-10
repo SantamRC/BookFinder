a88aa70dde5fd07d0222edc87f380a75943dc11f
SantamRC <santamdev404@gmail.com>
2021-07-10 21:11:27 +0530

    dotenv

--- src/Components/Add/Add.jsx
+++ src/Components/Add/Add.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
-import moment from 'moment';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -66,3 +65,4 @@ export default function Add() {
         </form>
     )
 }
+
